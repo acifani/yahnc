6cc9651650336d817084e95f939aab475ee79b98
Alessandro Cifani <alessandro.cifani@adevinta.com>
2022-11-05 22:35:10 +0100

    build: statically build the first 2 pages

--- app/[list]/[[...page]]/page.tsx
+++ app/[list]/[[...page]]/page.tsx
@@ -1,6 +1,8 @@
 import { notFound } from 'next/navigation';
 import { PostList, type Post } from '../../../components/PostList';
 
+const lists = ['news', 'ask', 'show', 'jobs'];
+
 async function fetchPosts(
   list: string,
   page: string | number
@@ -17,10 +19,23 @@ export default async function List({
 }: {
   params: { list: string; page?: string };
 }) {
-  if (!['news', 'ask', 'show', 'jobs'].includes(params.list)) {
+  if (!lists.includes(params.list)) {
     notFound();
   }
 
   const posts = await fetchPosts(params.list, params.page || 1);
   return <PostList posts={posts} />;
+}
+
+export async function generateStaticParams() {
+  const prerenderPages = ['', '1', '2'];
+  const staticParams = [];
+
+  for (const list of lists) {
+    for (const page of prerenderPages) {
+      staticParams.push({ list, page: [page] });
+    }
+  }
+
+  return staticParams;
 }
