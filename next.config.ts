7fef0e814071d803c864d25f82b829af6fd4c356
Alessandro Cifani <alessandro.cifani@adevinta.com>
2025-10-10 10:00:45 +0200

    chore: upgrade to next16

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
   experimental: {
-    reactCompiler: true,
-    ppr: 'incremental',
+    cacheComponents: true,
   },
 };
 
