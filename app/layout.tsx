19e28c1fc7c14b8eb39e630028be757071a6956a
Alessandro Cifani <alessandro.cifani@adevinta.com>
2025-12-10 14:15:30 +0100

    chore: bump deps

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,6 @@ export const metadata: Metadata = {
   description: 'YAHNC - Yet Another Hacker News Client',
 };
 
-export const experimental_ppr = true;
-
 export default function RootLayout({
   children,
 }: {
--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  experimental: {
-    cacheComponents: true,
-  },
+  cacheComponents: true,
 };
 
 export default nextConfig;
